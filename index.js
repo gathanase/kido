3e58da283a15d8d598158a99817e15e1daad6c40
Gregoire Athanase <gathanase@antidot.net>
2022-09-10 18:47:42 +0200

    Some refactoring

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
 avatar = {
   head: 1,
-  hands: 4,
+  hands: 2,
   body: 3,
-  feet: 1
+  feet: 4,
+  theme: 'xmas'
 }
 
 
-function drawAvatar() {
-  var canvas = document.getElementById("canvas");
-  var ratio = window.devicePixelRatio;
-  canvas.width = 500 * ratio;
-  canvas.height = 507 * ratio;
-  assets = 'assets/images/cute';
+avatarImage = {
+  canvas: document.getElementById("canvas"),
+  context: null,
+  init: function() {
+    var ratio = window.devicePixelRatio;
+    this.canvas.width = 500 * ratio;
+    this.canvas.height = 507 * ratio;
+    this.context = this.canvas.getContext("2d");
+    this.context.scale(ratio, ratio);
+  },
 
-  var context = canvas.getContext("2d");
-  context.scale(ratio, ratio);
-
-  var head = new Image();
-  head.onload = function (e) { context.drawImage(head, 0, 0, 500, 231); }
-  head.src = `${assets}/th${avatar.head}.jpg`;
-
-  var leftHand = new Image();
-  leftHand.onload = function (e) { context.drawImage(leftHand, 0, 231, 164, 77); }
-  leftHand.src = `${assets}/bd${avatar.hands}.jpg`;
-
-  var torso = new Image();
-  torso.onload = function (e) { context.drawImage(torso, 164, 231, 174, 77); }
-  torso.src = `${assets}/tb${avatar.head}.jpg`;
-
-  var rightHand = new Image();
-  rightHand.onload = function (e) { context.drawImage(rightHand, 164+174, 231, 162, 77); }
-  rightHand.src = `${assets}/bg${avatar.hands}.jpg`;
-
-  var body = new Image();
-  body.onload = function (e) { context.drawImage(body, 0, 231+77, 500, 113); }
-  body.src = `${assets}/c${avatar.body}.jpg`;
-
-  var feet = new Image();
-  feet.onload = function (e) { context.drawImage(feet, 0, 231+77+113, 500, 86); }
-  feet.src = `${assets}/p${avatar.feet}.jpg`;
+  paint: function(avatar) {
+    var assets = `assets/images/${avatar.theme}`;
+    var head = new Image();
+    var context = this.context
+    head.onload = function (e) { context.drawImage(head, 0, 0, 500, 231); }
+    head.src = `${assets}/th${avatar.head}.jpg`;
+  
+    var leftHand = new Image();
+    leftHand.onload = function (e) { context.drawImage(leftHand, 0, 231, 164, 77); }
+    leftHand.src = `${assets}/bd${avatar.hands}.jpg`;
+  
+    var torso = new Image();
+    torso.onload = function (e) { context.drawImage(torso, 164, 231, 174, 77); }
+    torso.src = `${assets}/tb${avatar.head}.jpg`;
+  
+    var rightHand = new Image();
+    rightHand.onload = function (e) { context.drawImage(rightHand, 164+174, 231, 162, 77); }
+    rightHand.src = `${assets}/bg${avatar.hands}.jpg`;
+  
+    var body = new Image();
+    body.onload = function (e) { context.drawImage(body, 0, 231+77, 500, 113); }
+    body.src = `${assets}/c${avatar.body}.jpg`;
+  
+    var feet = new Image();
+    feet.onload = function (e) { context.drawImage(feet, 0, 231+77+113, 500, 86); }
+    feet.src = `${assets}/p${avatar.feet}.jpg`;
+  }
 }
 
-drawAvatar();
+avatarImage.init();
+avatarImage.paint(avatar);
 
 
 var map = L.map('my-map', {zoomControl: false});
@@ -85,10 +92,6 @@ targets.forEach(target => {
         console.log(item);
         avatar[item.part] = item.animal;
         drawAvatar();
-        //popup
-        //    .setLatLng(L.latLng(target.lat, target.lon))
-        //    .setContent(target.code)
-        //    .openOn(map);
     }
     var marker = L.circle([target.lat, target.lon], {color: groups[target.group].color, fillOpacity: opacity});
     marker.on('click', onMarkerClick);
